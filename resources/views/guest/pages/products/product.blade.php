6bac4ad97585ce36d1bd9ededfd61516eaf0efe5
khoadev2021 <nguyenkhoa.offical2021@gmail.com>
2022-12-29 10:28:30 +0700

    thêm thẻ p

--- resources/views/guest/pages/products/product.blade.php
+++ resources/views/guest/pages/products/product.blade.php
@@ -6,5 +6,6 @@
 
 @section('user-contents')
     <h1>ABC</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis debitis minus, quaerat, veritatis consectetur illo itaque, pariatur sed sunt sint excepturi! Natus accusantium eos ad delectus. Pariatur aliquid a odit?</p>
 @endsection
 
