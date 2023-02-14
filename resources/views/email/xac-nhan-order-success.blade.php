aa504101dfe5961198fd8854b442ead1624c5b06
khoadev2021 <nguyenkhoa.offical2021@gmail.com>
2023-02-14 19:50:06 +0700

    14/02

---
+++ resources/views/email/xac-nhan-order-success.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <p>Cảm ơn bạn đã đặt hàng tại BCompter</p>
+    <h6>Hi! {{$user->fullname}} </h6>
+    <p>Lazada đã nhận được yêu cầu đặt hàng của bạn và đang xử lý nhé. Bạn sẽ nhận được thông báo tiếp theo khi đơn hàng đã sẵn sàng được giao.</p>
+    
+</div>
