90ce9c0ceb86a8357a7f2ffc97866908d3f4e823
Pontadev <nguyenkhoa.offical2408@gmail.com>
2023-02-18 03:52:58 +0700

    push code 18/02/2023

---
+++ resources/views/admin/pages/products/create-cauhinh.blade.php
@@ -0,0 +1,72 @@
+<div class="modal fade" id="create-cauhinh" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="create-cauhinh-header"></h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="#">
+                    <div class="mb-3 d-flex align-items-center gap-3">
+                        <label class="form-label" style="width:200px">CPU</label>
+                        <input type="email" class="form-control" id="cauhinh_cpu">
+                    </div>
+                    <div class="mb-3 d-flex align-items-center gap-3">
+                        <label class="form-label" style="width:200px">RAM</label>
+                        <input type="email" class="form-control" id="cauhinh_ram">
+                    </div>
+                    <div class="mb-3 d-flex align-items-center gap-3">
+                        <label class="form-label" style="width:200px">Storage
+                            drive</label>
+                        <input type="email" class="form-control" id="cauhinh_Storage">
+                    </div>
+                    <div class="mb-3 d-flex align-items-center gap-3">
+                        <label class="form-label" style="width:200px">Graphics
+                            card</label>
+                        <input type="email" class="form-control" id="cauhinh_graphic_card">
+                    </div>
+                    <div class="mb-3 d-flex align-items-center gap-3">
+                        <label class="form-label" style="width:200px">Screen
+                            display</label>
+                        <input type="email" class="form-control" id="cauhinh_screen">
+                    </div>
+                    <div class="mb-3 d-flex align-items-center gap-3">
+                        <label class="form-label" style="width:200px">Bàn phím</label>
+                        <input type="email" class="form-control" id="cauhinh_keyboard">
+                    </div>
+                    <div class="mb-3 d-flex align-items-center gap-3">
+                        <label class="form-label" style="width:200px">Audio</label>
+                        <input type="email" class="form-control" id="cauhinh_Audio">
+                    </div>
+                    <div class="mb-3 d-flex align-items-center gap-3">
+                        <label class="form-label" style="width:200px">Kết nối không
+                            dây</label>
+                        <input type="email" class="form-control" id="cauhinh_wireless">
+                    </div>
+                    <div class="mb-3 d-flex align-items-center gap-3">
+                        <label class="form-label" style="width:200px">Webcam</label>
+                        <input type="email" class="form-control" id="cauhinh_webcam">
+                    </div>
+                    <div class="mb-3 d-flex align-items-center gap-3">
+                        <label class="form-label" style="width:200px">Cổng giao
+                            tiếp</label>
+                        <input type="email" class="form-control" id="cauhinh_usb">
+                    </div>
+                    <div class="mb-3 d-flex align-items-center gap-3">
+                        <label class="form-label" style="width:200px">Hệ điều
+                            hành</label>
+                        <input type="email" class="form-control" id="cauhinh_window">
+                    </div>
+                    <div class="mb-3 d-flex align-items-center gap-3">
+                        <label for="exampleFormControlInput1" class="form-label" style="width:200px">Nguồn</label>
+                        <input type="email" class="form-control" id="cauhinh_power">
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" id="btn-submit">Save change</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
